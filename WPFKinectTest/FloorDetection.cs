d20f355f893ba8cd27ff5a637b0ebb5b01154a27
Sam Shteinman <sshte063@uottawa.ca>
2012-04-01 23:38:58 -0400

    Changed the bracket formatting and refactored some code.

--- WPFKinectTest/FloorDetection.cs
+++ WPFKinectTest/FloorDetection.cs
@@ -4,10 +4,8 @@ using System.Linq;
 using System.Text;
 using System.IO;
 
-namespace WPFKinectTest
-{
-    class FloorDetection
-    {
+namespace WPFKinectTest {
+    class FloorDetection {
 
         int mid = 360;
         public Boolean flatSurfaceUp = true;
@@ -35,30 +33,25 @@ namespace WPFKinectTest
         //Depth map loaded by test file
         int[,] providedDepthMap;
 
-        public FloorDetection()
-        {
+        public FloorDetection() {
             //Build Marc Andre's static depth map
             buildMarcSimulatedDepthMap();
             createZones();
         }
 
-        public byte[] execute(int[] realDepthArray)
-        {
+        public byte[] execute(int[] realDepthArray) {
             return checkIfFlatFloor(realDepthArray);
         }
 
         //================= PRIVATE METHODS =================================
 
-        private int[,] provideTestingDepthMapFromFile()
-        {
+        private int[,] provideTestingDepthMapFromFile() {
             //Testing Purposes
             TextReader txtReader = new StreamReader("TESTINGFILE.txt");
             int[,] providedDepthMap = new int[640, 480];
 
-            for (int x = 0; x < 640; x++)
-            {
-                for (int y = 0; y < 480; y++)
-                {
+            for (int x = 0; x < 640; x++) {
+                for (int y = 0; y < 480; y++) {
                     providedDepthMap[x, y] = Convert.ToInt32(txtReader.ReadLine());
                 }
             }
@@ -68,8 +61,7 @@ namespace WPFKinectTest
 
         //Here we save an image from the kinect camera so that we may run
         //automated tests later
-        private void saveDepthMapToFile(int[] depthArray)
-        {
+        private void saveDepthMapToFile(int[] depthArray) {
             //Here we write the depth map to a file for testing purposes
             TextWriter tw = new StreamWriter("TESTINGFILE.txt");
 
@@ -83,68 +75,55 @@ namespace WPFKinectTest
             providedDepthMap = provideTestingDepthMapFromFile();
         }
 
-        private byte[] convertDiffToColor(int[,] diffDepthArray)
-        {
+        private byte[] convertDiffToColor(int[,] diffDepthArray) {
 
             byte[] colorDepthArray = new byte[4 * 640 * 480];
 
-            for (int x = 0; x < 640; x++)
-            {
-                for (int y = 0; y < 480; y++)
-                {
+            for (int x = 0; x < 640; x++) {
+                for (int y = 0; y < 480; y++) {
                     int diff = diffDepthArray[x, y];
-                    if (y == 240 && (zoneArray[0, 0] <= x && zoneArray[3, 0] >= x))
-                    {
-                        colorDepthArray[4 * (x + y * 640) + RedIndex] = (byte)(flatSurfaceUp ? 0 : 255);
-                        colorDepthArray[4 * (x + y * 640) + GreenIndex] = (byte)(flatSurfaceUp ? 255 : 0);
-                        colorDepthArray[4 * (x + y * 640) + BlueIndex] = (byte)(flatSurfaceUp ? 0 : 255);
+                    int r = 0, g = 0, b = 0;
+                    if (y == 240 && (zoneArray[0, 0] <= x && zoneArray[3, 0] >= x)) {
+                        r = flatSurfaceUp ? 0 : 255;
+                        g = flatSurfaceUp ? 255 : 0;
+                        b = flatSurfaceUp ? 0 : 255;
+                    } else if (y == mid && (zoneArray[1, mid - 240] <= x && zoneArray[2, mid - 240] >= x)) {
+                        r = flatSurfaceDown ? 0 : 255;
+                        g = flatSurfaceDown ? 255 : 0;
+                        b = flatSurfaceDown ? 0 : 255;
+                    } else if (y >= 240 && (zoneArray[0, y - 240] == x || zoneArray[1, y - 240] == x)) {
+                        r = flatSurfaceLeft ? 0 : 255;
+                        g = flatSurfaceLeft ? 255 : 0;
+                        b = flatSurfaceLeft ? 0 : 255;
+                    } else if (y >= 240 && (zoneArray[2, y - 240] == x || zoneArray[3, y - 240] == x)) {
+                        r = flatSurfaceRight ? 0 : 255;
+                        g = flatSurfaceRight ? 255 : 0;
+                        b = flatSurfaceRight ? 0 : 255;
+                    } else if (Math.Abs(diff) < MaximumError) {
+                        r = 255;
+                        g = 255;
+                        b = 255;
+                    } else if (diff < -MaximumError) {
+                        int col = 200 - Math.Min(-diff, 200);
+                        r = 255;
+                        g = col;
+                        b = col;
+                    } else if (diff > MaximumError) {
+                        int col = 200 - Math.Min(diff, 200);
+                        r = col;
+                        g = col;
+                        b = 255;
                     }
-                    else if (y == mid && (zoneArray[1, mid - 240] <= x && zoneArray[2, mid - 240] >= x))
-                    {
-                        colorDepthArray[4 * (x + y * 640) + RedIndex] = (byte)(flatSurfaceDown ? 0 : 255);
-                        colorDepthArray[4 * (x + y * 640) + GreenIndex] = (byte)(flatSurfaceDown ? 255 : 0);
-                        colorDepthArray[4 * (x + y * 640) + BlueIndex] = (byte)(flatSurfaceDown ? 0 : 255);
-                    }
-                    else if (y >= 240 && (zoneArray[0, y - 240] == x || zoneArray[1, y - 240] == x))
-                    {
-                        colorDepthArray[4 * (x + y * 640) + RedIndex] = (byte)(flatSurfaceLeft ? 0 : 255);
-                        colorDepthArray[4 * (x + y * 640) + GreenIndex] = (byte)(flatSurfaceLeft ? 255 : 0);
-                        colorDepthArray[4 * (x + y * 640) + BlueIndex] = (byte)(flatSurfaceLeft ? 0 : 255);
-                    }
-                    else if (y >= 240 && (zoneArray[2, y - 240] == x || zoneArray[3, y - 240] == x))
-                    {
-                        colorDepthArray[4 * (x + y * 640) + RedIndex] = (byte)(flatSurfaceRight ? 0 : 255);
-                        colorDepthArray[4 * (x + y * 640) + GreenIndex] = (byte)(flatSurfaceRight ? 255 : 0);
-                        colorDepthArray[4 * (x + y * 640) + BlueIndex] = (byte)(flatSurfaceRight ? 0 : 255);
-                    }
-                    else if (Math.Abs(diff) < MaximumError)
-                    {
-                        colorDepthArray[4 * (x + y * 640) + RedIndex] = 255;
-                        colorDepthArray[4 * (x + y * 640) + GreenIndex] = 255;
-                        colorDepthArray[4 * (x + y * 640) + BlueIndex] = 255;
-                    }
-                    else if (diff < -MaximumError)
-                    {
-                        byte col = (byte) (200 - Math.Min(-diff, 200));
-                        colorDepthArray[4 * (x + y * 640) + RedIndex] = 255;
-                        colorDepthArray[4 * (x + y * 640) + GreenIndex] = col;
-                        colorDepthArray[4 * (x + y * 640) + BlueIndex] = col;
-                    }
-                    else if (diff > MaximumError)
-                    {
-                        byte col = (byte) (200 - Math.Min(diff, 200));
-                        colorDepthArray[4 * (x + y * 640) + RedIndex] = col;
-                        colorDepthArray[4 * (x + y * 640) + GreenIndex] = col;
-                        colorDepthArray[4 * (x + y * 640) + BlueIndex] = 255;
-                    }
+                    colorDepthArray[4 * (x + y * 640) + RedIndex] = (byte) r;
+                    colorDepthArray[4 * (x + y * 640) + GreenIndex] = (byte) g;
+                    colorDepthArray[4 * (x + y * 640) + BlueIndex] = (byte) b;
                 }
             }
 
             return colorDepthArray;
         }
 
-        private byte[] checkIfFlatFloor(int[] realDepthArray)
-        {
+        private byte[] checkIfFlatFloor(int[] realDepthArray) {
 
             if (!depthMapLoaded)//Get the testing depth map from our file
             {
@@ -152,18 +131,15 @@ namespace WPFKinectTest
             }
 
             //If we want to calibrate, then we save an image containing a flat surface
-            if (!savedFlag)
-            {
+            if (!savedFlag) {
                 saveDepthMapToFile(realDepthArray);
             }
 
 
 
             int[,] diffDepthArray = new int[640, 480];
-            for (int x = 0; x < 640; x++)
-            {
-                for (int y = 0; y < 480; y++)
-                {
+            for (int x = 0; x < 640; x++) {
+                for (int y = 0; y < 480; y++) {
                     diffDepthArray[639 - x, y] = realDepthArray[x + y * 640] - providedDepthMap[x, y];
                 }
             }
@@ -173,47 +149,29 @@ namespace WPFKinectTest
             int goodFlagDown = 0;
             int goodFlagLeft = 0;
             int goodFlagRight = 0;
-            for (int y = TOP; y < 480; y++)
-            {
-                for (int x = 0; x < 640; x++)
-                {
-                    if (x < zoneArray[0, y - TOP])
-                    {
+            for (int y = TOP; y < 480; y++) {
+                for (int x = 0; x < 640; x++) {
+                    if (x < zoneArray[0, y - TOP]) {
                         //Do nothing we are at the begin
-                    }
-                    else if (x >= zoneArray[0, y - TOP] && x <= zoneArray[1, y - TOP])
-                    {
-                        if (Math.Abs(diffDepthArray[x, y]) >= MaximumError)
-                        {
+                    } else if (x >= zoneArray[0, y - TOP] && x <= zoneArray[1, y - TOP]) {
+                        if (Math.Abs(diffDepthArray[x, y]) >= MaximumError) {
                             goodFlagLeft++;
                         }
-                    }
-                    else if (x > zoneArray[1, y - TOP] && x < zoneArray[2, y - TOP])
-                    {
-                        if (y < mid)
-                        {
-                            if (Math.Abs(diffDepthArray[x, y]) >= MaximumError)
-                            {
+                    } else if (x > zoneArray[1, y - TOP] && x < zoneArray[2, y - TOP]) {
+                        if (y < mid) {
+                            if (Math.Abs(diffDepthArray[x, y]) >= MaximumError) {
                                 goodFlagUp++;
                             }
-                        }
-                        else
-                        {
-                            if (Math.Abs(diffDepthArray[x, y]) >= MaximumError)
-                            {
+                        } else {
+                            if (Math.Abs(diffDepthArray[x, y]) >= MaximumError) {
                                 goodFlagDown++;
                             }
                         }
-                    }
-                    else if (x >= zoneArray[2, y - TOP] && x <= zoneArray[3, y - TOP])
-                    {
-                        if (Math.Abs(diffDepthArray[x, y]) >= MaximumError)
-                        {
+                    } else if (x >= zoneArray[2, y - TOP] && x <= zoneArray[3, y - TOP]) {
+                        if (Math.Abs(diffDepthArray[x, y]) >= MaximumError) {
                             goodFlagRight++;
                         }
-                    }
-                    else if (x > zoneArray[3, y - TOP])
-                    {
+                    } else if (x > zoneArray[3, y - TOP]) {
                         //We reach the end of the testing zone.
                         break;
                     }
@@ -232,8 +190,7 @@ namespace WPFKinectTest
 
         }
 
-        private void buildMarcSimulatedDepthMap()
-        {
+        private void buildMarcSimulatedDepthMap() {
             int Width = 640;
             int Height = 480;
 
@@ -248,10 +205,8 @@ namespace WPFKinectTest
 
             int[,] depthArray = new int[640, 480];
 
-            for (int i = 0; i < Width; i++)
-            {
-                for (int j = 0; j < Height; j++)
-                {
+            for (int i = 0; i < Width; i++) {
+                for (int j = 0; j < Height; j++) {
                     double currentVertAngle = angleKinect - (verticalWide / 2.0) * ((j - TOP) / (480.0 - TOP));
                     double radVertAngle = currentVertAngle * Math.PI / 180.0;
                     double calculatedDistance = heightKinect / Math.Cos(radVertAngle);
@@ -260,13 +215,11 @@ namespace WPFKinectTest
                     double radHorAngle = currentHorAngle / 180.0 * Math.PI;
                     double finalDistance = calculatedDistance / Math.Cos(radHorAngle);
 
-                    depthArray[i, j] = (int)finalDistance;
+                    depthArray[i, j] = (int) finalDistance;
                 }
             }
-            for (int x = 0; x < Width; x++)
-            {
-                for (int j = TOP; j < Height; j++)
-                {
+            for (int x = 0; x < Width; x++) {
+                for (int j = TOP; j < Height; j++) {
                     double currentVertAngle = angleKinect - (verticalWide / 2.0) * ((j - TOP) / (480.0 - TOP));
                     double radVertAngle = currentVertAngle * Math.PI / 180.0;
                     double calculatedDistance = heightKinect / Math.Cos(radVertAngle);
@@ -275,10 +228,9 @@ namespace WPFKinectTest
                     double radHorAngle = currentHorAngle / 180.0 * Math.PI;
                     double finalDistance = calculatedDistance / Math.Cos(radHorAngle);
                     double horDistance = finalDistance * Math.Sin(radHorAngle);
-                    int finalValue = (int)horDistance;
+                    int finalValue = (int) horDistance;
 
-                    if (Math.Abs(Math.Abs(finalValue) - radius) < 3)
-                    {
+                    if (Math.Abs(Math.Abs(finalValue) - radius) < 3) {
                         depthArray[x, j] = -2;
                     }
                 }
@@ -288,31 +240,21 @@ namespace WPFKinectTest
             marcSimulatedDepthArray = depthArray;
         }
 
-        private void createZones()
-        {
+        private void createZones() {
 
-            for (int y = TOP; y < 480; y++)
-            {
+            for (int y = TOP; y < 480; y++) {
                 int startDetect = 0;
-                for (int x = 0; x < 640; x++)
-                {
+                for (int x = 0; x < 640; x++) {
                     int value = marcSimulatedDepthArray[x, y];
-                    if (value == -2 && startDetect == 0)
-                    {
+                    if (value == -2 && startDetect == 0) {
                         startDetect = 1;
-                    }
-                    else if (!(value == -2) && startDetect == 1)
-                    {
+                    } else if (!(value == -2) && startDetect == 1) {
                         startDetect = 2;
                         zoneArray[0, y - TOP] = x;
                         zoneArray[1, y - TOP] = x + getZoneSize(y);
-                    }
-                    else if (value != -2 && startDetect == 2)
-                    {
+                    } else if (value != -2 && startDetect == 2) {
 
-                    }
-                    else if (marcSimulatedDepthArray[x, y] == -2 && startDetect == 2)
-                    {
+                    } else if (marcSimulatedDepthArray[x, y] == -2 && startDetect == 2) {
                         zoneArray[3, y - TOP] = x - 1;
                         zoneArray[2, y - TOP] = x - 1 - getZoneSize(y);
                         break;
@@ -322,15 +264,14 @@ namespace WPFKinectTest
 
         }
 
-        private int getZoneSize(int y)
-        {
+        private int getZoneSize(int y) {
             int maxSize = 50;
             int minSize = 0;
             int dif = maxSize - minSize;
-            int result = (int)(minSize + dif * (480.0 - y) / TOP);
+            int result = (int) (minSize + dif * (480.0 - y) / TOP);
             return result;
         }
 
     }
-   
+
 }
