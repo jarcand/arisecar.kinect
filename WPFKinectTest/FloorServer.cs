07c6ca93c2d411d828594a55906e05cbef9b6fa9
Sam Shteinman <sshte063@uottawa.ca>
2012-04-03 20:06:29 -0400

    Fix to server for changing commmands, removed the *.suo files from the repo.

--- WPFKinectTest/FloorServer.cs
+++ WPFKinectTest/FloorServer.cs
@@ -6,29 +6,24 @@ using System.Threading;
 using System.IO;
 using System.Net;
 
-namespace WPFKinectTest
-{
-    class FloorServer
-    {
+namespace WPFKinectTest {
+    class FloorServer {
         //Server handling client requests asking for flat surface
         System.Net.Sockets.TcpListener Server;
 
         FloorDetection floorDetection;
 
-        public FloorServer(FloorDetection floorDetection)
-        {
+        public FloorServer(FloorDetection floorDetection) {
             this.floorDetection = floorDetection;
             Thread thread = new Thread(new ThreadStart(AcceptClients));
             thread.Start();
         }
 
-        private void AcceptClients()
-        {
+        private void AcceptClients() {
             Server = new System.Net.Sockets.TcpListener(IPAddress.Any, 1234);
             Console.WriteLine("\nWaiting for Clients");
             Server.Start();
-            while (true)
-            {
+            while (true) {
                 System.Net.Sockets.TcpClient chatConnection = Server.AcceptTcpClient();
                 Thread thread = new Thread(communicateWithClient);
                 thread.Start(chatConnection);
@@ -38,29 +33,25 @@ namespace WPFKinectTest
 
         int connections = 0;
 
-        private void communicateWithClient(object givenChatConnection)
-        {
-            System.Net.Sockets.TcpClient chatConnection = (System.Net.Sockets.TcpClient)givenChatConnection;
+        private void communicateWithClient(object givenChatConnection) {
+            System.Net.Sockets.TcpClient chatConnection = (System.Net.Sockets.TcpClient) givenChatConnection;
             connections++;
             Console.WriteLine("Someone connected!  Connection count is " + connections);
             StreamReader inputStream = new System.IO.StreamReader(chatConnection.GetStream());
             StreamWriter outputStream = new System.IO.StreamWriter(chatConnection.GetStream());
             Console.WriteLine("Reader stream");
-            try
-            {
+            try {
                 int input = 0;
-                while (input != -1)
-                {
+                while (input != -1) {
                     input = inputStream.Read();
-                    switch (input)
-                    {
+                    switch (input) {
                         case 'a':
                             //Console.WriteLine("Received request: " + flatSurface);
                             int resp = (floorDetection.flatSurfaceDown ? 1 : 0) << 0
                                 | (floorDetection.flatSurfaceUp ? 1 : 0) << 1
                                 | (floorDetection.flatSurfaceLeft ? 1 : 0) << 2
                                 | (floorDetection.flatSurfaceRight ? 1 : 0) << 3;
-                            char respB = (char)resp;
+                            char respB = (char) resp;
                             outputStream.Write(respB);
                             outputStream.Flush();
                             break;
@@ -68,14 +59,18 @@ namespace WPFKinectTest
                             floorDetection.savedFlag = false;
                             break;
                         case 'e':
-                            int newMaxE = inputStream.Read();
-                            if (0 <= newMaxE && newMaxE <= 255) {
+                            int newMaxE1 = inputStream.Read();
+                            int newMaxE2 = inputStream.Read();
+                            int newMaxE = newMaxE2 << 7 | newMaxE1;
+                            if (0 <= newMaxE && newMaxE <= 16383) {
                                 floorDetection.MaximumError = newMaxE;
                             }
                             break;
                         case 'i':
-                            int newMinI = inputStream.Read();
-                            if (0 <= newMinI && newMinI <= 255) {
+                            int newMinI1 = inputStream.Read();
+                            int newMinI2 = inputStream.Read();
+                            int newMinI = newMinI2 << 7 | newMinI1;
+                            if (0 <= newMinI && newMinI <= 16383) {
                                 floorDetection.MinimumInteferance = newMinI;
                             }
                             break;
@@ -86,9 +81,7 @@ namespace WPFKinectTest
                             break;
                     }
                 }
-            }
-            catch (IOException e)
-            {
+            } catch (IOException e) {
                 Console.WriteLine("IOException: " + e);
             }
             connections--;
